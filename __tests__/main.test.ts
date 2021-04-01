09e90c5676990f7f23434cee2b528b4f1b15e206
Louis Boudreau <l.boudreau@agendrix.com>
2021-03-31 20:58:55 -0400

    Create action to monitor ecs service deployments

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,10 +1,9 @@
-import { wait } from "../src/wait";
 import * as process from "process";
 import * as cp from "child_process";
 import * as path from "path";
 
 test("throws invalid number", async () => {
-  const input = parseInt("foo", 10);
+  const input = parseInt("foo", 10)number;
   await expect(wait(input)).rejects.toThrow("milliseconds not a number");
 });
 
@@ -14,15 +13,4 @@ test("wait 500 ms", async () => {
   const end = new Date();
   var delta = Math.abs(end.getTime() - start.getTime());
   expect(delta).toBeGreaterThan(450);
-});
-
-// shows how the runner will run a javascript action with env / stdout protocol
-test("test runs", () => {
-  process.env["INPUT_MILLISECONDS"] = "500";
-  const np = process.execPath;
-  const ip = path.join(__dirname, "..", "lib", "src", "main.js");
-  const options: cp.ExecFileSyncOptions = {
-    env: process.env
-  };
-  console.log(cp.execFileSync(np, [ip], options).toString());
 });
